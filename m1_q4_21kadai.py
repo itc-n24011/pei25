90c09813445636d46828f6ac5684c17789d13fdf
itc-n24011 <n24011@std.it-college.ac.jp>
2026-01-29 02:05:42 +0000

    kadai

---
+++ m1_q4_21kadai.py
@@ -0,0 +1,16 @@
+list_s = [1, 2, 3]
+num = 6
+print(num)
+
+num = num // 2
+print(num)
+
+num = list_s[-1]
+print(num)
+
+num = list_s[0]
+print(num)
+
+list_s.remove(1)
+print(list_s)
+
---
+++ m1_q4_22kadai.py
@@ -0,0 +1,5 @@
+all_mun = [17, 5, 3, 2, 9]
+
+print(f"{min(all_mun)}, {max(all_mun)}, {sum(all_mun)}")
+print(all_mun)
+
